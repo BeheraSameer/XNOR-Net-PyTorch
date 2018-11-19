fdb20ba6f99aaa9b87cadfb55b00ea792e09cf8e
Sameer Kumar Behera <sameer-tamu@tamu.edu>
2018-11-19 22:20:29 +0000

    Batch Size

--- IMDB/main.py
+++ IMDB/main.py
@@ -18,3 +18,13 @@ train_data, valid_data = train_data.split(random_state=random.seed(SEED))
 
 TEXT.build_vocab(train_data, max_size=25000, vectors="glove.6B.100d")
 LABEL.build_vocab(train_data)
+
+BATCH_SIZE = 64
+
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+train_iterator, valid_iterator, test_iterator = data.BucketIterator.splits(
+    (train_data, valid_data, test_data), 
+    batch_size=BATCH_SIZE, 
+    device=device)
+
