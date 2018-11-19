f7d846a4cd14e1b8d102d6e510be906bdf0d73fa
Sameer Kumar Behera <sameer-tamu@tamu.edu>
2018-11-19 22:13:00 +0000

    SEED

--- IMDB/main.py
+++ IMDB/main.py
@@ -2,3 +2,16 @@ import torch
 from torchtext import data
 from torchtext import datasets
 import random
+
+SEED = 1234
+
+torch.manual_seed(SEED)
+torch.cuda.manual_seed(SEED)
+torch.backends.cudnn.deterministic = True
+
+TEXT = data.Field(tokenize='spacy')
+LABEL = data.LabelField(dtype=torch.float)
+
+train_data, test_data = datasets.IMDB.splits(TEXT, LABEL)
+
+train_data, valid_data = train_data.split(random_state=random.seed(SEED))
