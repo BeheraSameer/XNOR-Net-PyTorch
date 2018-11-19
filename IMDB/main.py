2a8e43cc2c6c7cd75bae0456797d144381224767
Sameer Kumar Behera <sameer-tamu@tamu.edu>
2018-11-19 22:50:00 +0000

    Forward Def

--- IMDB/main.py
+++ IMDB/main.py
@@ -39,4 +39,21 @@ class CNN(nn.Module):
         self.convs = nn.ModuleList([nn.Conv2d(in_channels=1, out_channels=n_filters, kernel_size=(fs,embedding_dim)) for fs in filter_sizes])
         self.fc = nn.Linear(len(filter_sizes)*n_filters, output_dim)
         self.dropout = nn.Dropout(dropout)
+    
+    def forward(self, x):
+        #x = [sent len, batch size]
+        x = x.permute(1, 0)
+        #x = [batch size, sent len]
+        embedded = self.embedding(x)
+        #embedded = [batch size, sent len, emb dim]
+        embedded = embedded.unsqueeze(1)
+        #embedded = [batch size, 1, sent len, emb dim]
+        conved = [F.relu(conv(embedded)).squeeze(3) for conv in self.convs]
+        #conv_n = [batch size, n_filters, sent len - filter_sizes[n]]
+        pooled = [F.max_pool1d(conv, conv.shape[2]).squeeze(2) for conv in conved]
+        #pooled_n = [batch size, n_filters]
+        cat = self.dropout(torch.cat(pooled, dim=1))
+        #cat = [batch size, n_filters * len(filter_sizes)]
+            
+        return self.fc(cat)
 
