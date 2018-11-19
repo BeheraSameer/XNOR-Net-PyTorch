5ddb58c89bd86fe3f320e4df62c85e61282115a1
Sameer Kumar Behera <sameer-tamu@tamu.edu>
2018-11-19 22:17:58 +0000

    Build Vocab

--- IMDB/main.py
+++ IMDB/main.py
@@ -15,3 +15,6 @@ LABEL = data.LabelField(dtype=torch.float)
 train_data, test_data = datasets.IMDB.splits(TEXT, LABEL)
 
 train_data, valid_data = train_data.split(random_state=random.seed(SEED))
+
+TEXT.build_vocab(train_data, max_size=25000, vectors="glove.6B.100d")
+LABEL.build_vocab(train_data)
